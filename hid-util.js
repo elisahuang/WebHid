76f524734465139c33ad20b672cd50cf9f57a6ff
elisahuang <elisa.huang@luxshare-ict.com>
2021-03-03 10:56:57 +0800

    add hid connect

--- hid-util.js
+++ hid-util.js
@@ -2,12 +2,29 @@
 var usbDevice = null;
 let outputReport = new Uint8Array([42]);
 document.addEventListener("DOMContentLoaded", async() => {
-
-  let devices = await navigator.registerProtocolHandler.getDevices();
+  let btnConnect = document.getElementById("connect");
+  btnConnect.addEventListener("click", connectHID);
+ /* let devices = await navigator.registerProtocolHandler.getDevices();
   devices.array.forEach(devices => {
     console.log(`HID: ${device.productName}`);
-  });
+  });*/
 });
+const connectHID = async () => {
+  console.log("connectHID");
+  const filters = [];
+  try{
+    usbDevice = await navigator.hid.requestDevice({ filters: filters });
+    if (usbDevice){
+      console.log("Product name: " + usbDevice.productName);  
+      console.log("productId:"+ usbDevice.productId);  
+    }
+    else{
+      console.log("There is no device.");
+    }
+  }catch(error){
+    console.error(error.name, error.message);
+  }
+}
 navigator.hid.addEventListener('connect', ({device}) => {
   console.log(`HID connected: ${device.productName}`);
 });
@@ -15,21 +32,26 @@ navigator.hid.addEventListener('connect', ({device}) => {
 navigator.hid.addEventListener('disconnect', ({device}) => {
   console.log(`HID disconnected: ${device.productName}`);
 });
-let requestButton = document.getElementById("connect");
+/*let requestButton = document.getElementById("connect");
 requestButton.addEventListener("click", async () => {
+  console.log("connect"); 
   let device;
+  let filters = [];
   if ("hid" in navigator) {
     // The WebHID API is supported.
   }
   try {
     const devices = await navigator.hid.requestDevice({
-      filters: [
-        {
-          vendorId: 0x483, //  STM32
-          productId: 0x5740,
-        },
-      ],
+      filters: filters
     });
+    if (devices){
+      console.log("Product name: " + udevices.productName);  
+      console.log("productId:"+ devices.productId);  
+      $( "div.success" ).fadeIn( 300 ).delay( 1500 ).fadeOut( 400 );
+    }
+    else{
+      console.log("There is no device.");
+    }
     device = devices[0];
   } catch (error) {
     console.log("An error occurred.");
@@ -39,4 +61,4 @@ requestButton.addEventListener("click", async () => {
   } else {
     console.log(`HID: ${device.productName}`);
   }
-});
+});*/
